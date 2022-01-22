423abffbed671c0066439c763ea86e1ad7397648
Faizal <faizalkpb@gmail.com>
2022-01-22 16:28:09 +0300

    update sign page and whtsap link

--- js/script.js
+++ js/script.js
@@ -17,3 +17,53 @@ $(document).ready(function() {
         } 
     });  
   });
+
+  $(document).ready(function() {
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth2').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  $(document).ready(function() {
+    $('#autoWidth3').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth3').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  $(document).ready(function() {
+    $('#autoWidth4').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth4').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  $(document).ready(function() {
+    $('#autoWidth5').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth5').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  $(document).ready(function() {
+    $('#autoWidth6').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth6').removeClass('cS-hidden');
+        } 
+    });  
+  });
